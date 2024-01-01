e27cb34d425607d2928cf3fbaa4f176df762bb94
Sayakdutt <sayakofficial01@gmail.com>
2024-01-01 23:47:34 +0530

    File Upload Form

--- components/upload/UploadForm.tsx
+++ components/upload/UploadForm.tsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import AlertMsg from "./AlertMsg";
+import { ToastContainer, toast } from "react-toastify";
+
+import "react-toastify/dist/ReactToastify.css";
+import FilePreview from "./FilePreview";
+
 const UploadForm = () => {
+  const [file, setFile] = useState<File | null>();
+  const [errorMsg, setErrorMsg] = useState<boolean>(false);
+
+  const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const fileList: FileList | null = event.target.files;
+    if (fileList) {
+      const files: File = fileList[0];
+      console.log(files);
+      if (files.size > 2000000) {
+        setErrorMsg(true);
+        console.warn("Please upload a file less than 2MB");
+
+        toast.warn("⚠️ Please upload a file less than 2MB!", {
+          position: "top-center",
+          autoClose: 6000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        return;
+      }
+      setErrorMsg(false);
+      setFile(files);
+    }
+  };
   return (
     <div className="text-center">
       <div className="flex items-center justify-center w-full">
@@ -25,13 +60,37 @@ const UploadForm = () => {
               <strong className="text-primary">drop</strong>
             </p>
             <p className="text-xs text-gray-500">
-              SVG, PNG, JPG or GIF (MAX. 800x400px)
+              SVG, PNG, JPG or GIF (Max Size: 2MB)
             </p>
           </div>
-          <input id="dropzone-file" type="file" className="hidden" />
+          <input
+            id="dropzone-file"
+            type="file"
+            className="hidden"
+            onChange={handleFileChange}
+          />
         </label>
       </div>
-      <button disabled className="p-2 bg-primary text-white w-[30%] rounded-full mt-5 hover:bg-green-400">
+      <ToastContainer
+        position="top-center"
+        autoClose={6000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {errorMsg ? <AlertMsg /> : null}
+
+      
+      {file ? <FilePreview file={file} removeFile={() => setFile(null)} /> : null}
+      <button
+        disabled={!file}
+        className="p-2 bg-primary text-white w-[30%] rounded-full mt-5 hover:bg-green-400 disabled:bg-gray-400"
+      >
         Upload
       </button>
     </div>
