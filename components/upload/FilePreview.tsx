7aed928ad9d7b884385573df5c02835e6345b989
Sayakdutt <sayakofficial01@gmail.com>
2024-01-10 01:24:03 +0530

    applied props types

--- components/upload/FilePreview.tsx
+++ components/upload/FilePreview.tsx
@@ -1,17 +1,18 @@
+import React from "react";
 import { FaRegFileAlt } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 
-type FilePreviewProps = {
+interface FilePreviewProps {
   file: File;
-  removeFile:()=> any;
+  removeFile: () => void;
 }
 
-const FilePreview = ({ file, removeFile }: FilePreviewProps) => {
+const FilePreview: React.FC<FilePreviewProps> = ({ file, removeFile }) => {
   return (
     <div className="flex items-center justify-between mt-5 border rounded-md p-2 border-green-200 gap-2">
       <div className="flex items-center p-2">
         <FaRegFileAlt size={50} className="text-green-500" />
-        <div className="text-left">
+        <div className="text-left ml-2">
           <h2 className="font-bold">{file?.name}</h2>
           <h2 className="text-[12px] text-gray-400 font-bold">
             {file?.type} / {(file?.size / 1024 / 1024).toFixed(2)}MB
